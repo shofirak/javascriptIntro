ad7bfecaba796c66a7e957e91c4084359c40ac02
shofirak <shofirkn@gmail.com>
2020-08-08 12:11:39 +0700

    initial commit

---
+++ intro1/task1.js
@@ -0,0 +1,42 @@
+// Membuat object
+const biodata = {
+  name: "Shofira Khairunnisa",
+  age: 18,
+  hobbies: ["Menonton film", "menggambar"],
+  isMarried: false,
+  schoolList: [{
+      name: "MI Fathan Mubina",
+      yearIn: 2011,
+      yearOut: 2015,
+      major: null
+    },
+    {
+      name: "SMPIT Raudhatul Jannah",
+      yearIn: 2015,
+      yearOut: 2017,
+      major: null
+    },
+    {
+      name: "SMK Wikrama Bogor",
+      yearIn: 2017,
+      yearOut: 2020,
+      major: "RPL"
+    }
+  ],
+  skills: [{
+      skillName: "HTML CSS",
+      level: "advanced"
+    },
+    {
+      skillName: "PHP",
+      level: "advanced"
+    },
+    {
+      skillName: "MYSQL",
+      level: "advanced"
+    }
+  ],
+  interestInCoding: true
+};
+
+console.log(biodata);
