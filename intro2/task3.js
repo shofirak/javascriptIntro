7ce0682836bfb15f87e21c7f4c78b80e0cfe7111
shofirak <shofirkn@gmail.com>
2020-08-12 11:48:59 +0700

    revisi data array

--- intro2/task3.js
+++ intro2/task3.js
@@ -3,6 +3,8 @@
 const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
   if (nilaiAwal >= nilaiAkhir) {
     console.log("Nilai akhir harus lebih besar dari nilai awal!");
+  } else if (dataArray.length < 5) {
+    console.log("Data array kurang dari lima!");
   } else {
     let seleksi = dataArray.filter(
       (value) => value > nilaiAwal && value < nilaiAkhir
