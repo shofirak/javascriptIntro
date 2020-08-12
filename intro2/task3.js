66a800e468a3c048231bb1cdeb0a8a7873888b91
shofirak <shofirkn@gmail.com>
2020-08-12 09:23:20 +0700

    rev

--- intro2/task3.js
+++ intro2/task3.js
@@ -4,14 +4,16 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
   if (nilaiAwal >= nilaiAkhir) {
     console.log("Nilai akhir harus lebih besar dari nilai awal!");
   } else {
-    let seleksi = dataArray.filter(value => value > nilaiAwal && value < nilaiAkhir);
+    let seleksi = dataArray.filter(
+      (value) => value > nilaiAwal && value < nilaiAkhir
+    );
     // pengkondisian ada tidaknya output
-    if (seleksi != '') {
-      console.log(seleksi.sort((a, b) => a - b))
+    if (seleksi != "") {
+      console.log(seleksi.sort((a, b) => a - b));
     } else {
-      console.log("Tidak ada output yang ditampilkan")
+      console.log("Tidak ada output yang ditampilkan");
     }
   }
-}
+};
 
 seleksiNilai(5, 20, [2, 25, 14, 17, 4, 30, 8]);
