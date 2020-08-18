10c0538ffcb1528a58f2c5e4d17f9186fc4f42a1
shofirak <shofirkn@gmail.com>
2020-08-18 09:25:46 +0700

    add task3 soal 2

--- intro3/task3.js
+++ intro3/task3.js
@@ -1,46 +1,46 @@
 // buat 2 program bebas dengan promise
 
 // 1. tentukan kelulusan jika rata' nilai >= 75
-const nilai = [80, 75, 90, 77];
+// const nilai = [80, 75, 90, 77];
 
-const seleksi = new Promise((resolve, reject) => {
-  const result = nilai.reduce((a, b) => a + b) / nilai.length;
+// const seleksi = new Promise((resolve, reject) => {
+//   const result = nilai.reduce((a, b) => a + b) / nilai.length;
 
-  if (result >= 75) {
-    resolve(result);
-  } else {
-    reject(result);
-  }
-});
+//   if (result >= 75) {
+//     resolve(result);
+//   } else {
+//     reject(result);
+//   }
+// });
 
-seleksi
-  .then((response) => {
-    console.log(`Selamat, Anda dinyatakan lulus! dengan rata-rata nilai ${response}`);
-  })
-  .catch((error) => {
-    console.log(`Maaf Anda harus mengulang kelas karena rata-rata nilai ${error}`);
-  });
+// seleksi
+//   .then((response) => {
+//     console.log(`Selamat, Anda dinyatakan lulus! dengan rata-rata nilai ${response}`);
+//   })
+//   .catch((error) => {
+//     console.log(`Maaf Anda harus mengulang kelas karena rata-rata nilai ${error}`);
+//   });
 
 // 2. search name
 
-// const name = ["Budi", "Dodi", "Alek", "Caca", "Edi"];
+const name = ["Budi", "Dodi", "Alek", "Caca", "Edi"];
 
-// const searchName = (a) => {
-//   new Promise((resolve, reject) => {
-//     const show = name.filter((value) => value.includes(a));
+const searchName = (value) => {
+  return new Promise((resolve, reject) => {
+    const show = name.filter((a) => a.includes(value));
 
-//     if (show) {
-//       resolve(show);
-//     } else {
-//       reject("no data displayed");
-//     }
-//   });
-// };
+    if (show != "") {
+      resolve(show);
+    } else {
+      reject("no data displayed");
+    }
+  });
+};
 
-// searchName("l")
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+searchName("ud")
+  .then((response) => {
+    console.log(response.join());
+  })
+  .catch((error) => {
+    console.log(error);
+  });
