ee464b11fbf9abc8e764079288f7099522cf4d74
shofirak <shofirkn@gmail.com>
2020-08-18 07:30:06 +0700

    revisi task3

--- intro3/task3.js
+++ intro3/task3.js
@@ -1,46 +1,46 @@
 // buat 2 program bebas dengan promise
 
 // 1. tentukan kelulusan jika rata' nilai >= 75
-// const nilai = [80, 75, 90, 77];
+const nilai = [80, 75, 90, 77];
 
-// const seleksi = new Promise((resolve, reject) => {
-//   const result = nilai.reduce((a, b) => a + b) / 4;
+const seleksi = new Promise((resolve, reject) => {
+  const result = nilai.reduce((a, b) => a + b) / nilai.length;
 
-//   if (result >= 75) {
-//     resolve(result);
-//   } else {
-//     reject(result);
-//   }
-// });
+  if (result >= 75) {
+    resolve(result);
+  } else {
+    reject(result);
+  }
+});
 
-// seleksi
-//   .then((response) => {
-//     console.log(`Selamat, Anda dinyatakan lulus! dengan rata-rata nilai ${response}`);
-//   })
-//   .catch((error) => {
-//     console.log(`Maaf Anda harus mengulang kelas dengan rata-rata nilai ${error}`);
-//   });
+seleksi
+  .then((response) => {
+    console.log(`Selamat, Anda dinyatakan lulus! dengan rata-rata nilai ${response}`);
+  })
+  .catch((error) => {
+    console.log(`Maaf Anda harus mengulang kelas karena rata-rata nilai ${error}`);
+  });
 
 // 2. search name
 
-const name = ["Budi", "Dodi", "Alek", "Caca", "Edi"];
+// const name = ["Budi", "Dodi", "Alek", "Caca", "Edi"];
 
-const searchName = (a) => {
-  new Promise((resolve, reject) => {
-    const show = name.filter((value) => value.includes(a));
+// const searchName = (a) => {
+//   new Promise((resolve, reject) => {
+//     const show = name.filter((value) => value.includes(a));
 
-    if (!show) {
-      reject("no data displayed");
-    } else {
-      resolve(show);
-    }
-  });
-};
+//     if (show) {
+//       resolve(show);
+//     } else {
+//       reject("no data displayed");
+//     }
+//   });
+// };
 
-searchName("Alek")
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// searchName("l")
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
