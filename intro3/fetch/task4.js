9f5873ba97a1a354df45c3bedeb3c043fa057cf7
shofirak <shofirkn@gmail.com>
2020-08-19 10:36:36 +0700

    revisi task4 mengunakan destructuring dan map

--- intro3/fetch/task4.js
+++ intro3/fetch/task4.js
@@ -3,10 +3,24 @@
 const fetch = require("node-fetch");
 
 fetch("https://jsonplaceholder.typicode.com/users")
-  .then((res) => res.json())
-  // .then((res) => {
-  //   for (let i = 0; i < res.length; i++) {
-  //     console.log(res[i].name)
-  //   }
-  // })
-  .then(res => res.map(e => console.log(e.name)))
+  // menggunakan map destructuring  
+  .then((res) => res.json()
+    .then(result => {
+      result.map((item) => {
+        const {
+          name
+        } = item
+        console.log(name)
+      })
+    }).catch((err) => console.log(err))
+  )
+
+// menggunakan map
+// .then(res => res.map(e => console.log(e.name)))
+
+// menggunakan looping
+// .then((res) => {
+//   for (let i = 0; i < res.length; i++) {
+//     console.log(res[i].name)
+//   }
+// })
