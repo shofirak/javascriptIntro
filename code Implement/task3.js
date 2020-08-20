17114b1449c2c0174a49790ca15df2958e9e19a8
shofirak <shofirkn@gmail.com>
2020-08-20 08:24:40 +0700

    revisi task3

--- code Implement/task3.js
+++ code Implement/task3.js
@@ -1,22 +1,33 @@
-// 0 sebagai pemisah dan urutkan bilangan berdasar bilangan itu sendiri
+// 0 sebagai pemisah dan urutkan bilangan berdasar kelompok tersebut
 
 const divideAndSort = (num) => {
   // pengkondisian input berupa number
   if (typeof num === "number") {
     const newNumber = num.toString().split(0);
 
-    for (let i = 0; i < newNumber.length; i++) {
-      console.log(
-        newNumber[i]
-          .toString()
-          .split("")
-          .sort((a, b) => a - b)
-          .join("")
-      );
-    }
+    const sequence = newNumber.map((a) =>
+      a
+        .toString()
+        .split("")
+        .sort((a, b) => a - b)
+        .join("")
+    );
+
+    console.log(sequence.join(""));
   } else {
     console.log("Data yang diinputkan bukan number!");
   }
 };
 
 divideAndSort(5956560159466056);
+
+// contoh penggunaan dengan for
+// for (let i = 0; i < newNumber.length; i++) {
+//   console.log(
+//     newNumber[i]
+//       .toString()
+//       .split("")
+//       .sort((a, b) => a - b)
+//       .join("")
+//   );
+// }
